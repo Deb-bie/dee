31177d5a4a2d907cb1064c83699bbe2aeaaf6834
Debbie <asamoahjaeldeborah@gmail.com>
2022-07-24 20:39:58 +0000

    Dummy Data

---
+++ src/data/Data.js
@@ -0,0 +1,291 @@
+export const Data = {
+    productItems: [
+      {
+        id: 1,
+        discount: 50,
+        cover: "./images/flash/flash-1.png",
+        name: "Shoes",
+        price: 100,
+      },
+      {
+        id: 2,
+        discount: 40,
+        cover: "./images/flash/flash-2.png",
+        name: "Watch",
+        price: 20,
+      },
+      {
+        id: 3,
+        discount: 40,
+        cover: "./images/flash/flash-3.png",
+        name: "Smart Mobile Black",
+        price: 200,
+      },
+      {
+        id: 4,
+        discount: 40,
+        cover: "./images/flash/flash-4.png",
+        name: "Smart Watch Black",
+        price: 50,
+      },
+      {
+        id: 5,
+        discount: 50,
+        cover: "./images/flash/flash-1.png",
+        name: "Shoes",
+        price: 100,
+      },
+      {
+        id: 6,
+        discount: 50,
+        cover: "./images/flash/flash-3.png",
+        name: "Shoes",
+        price: 100,
+      },
+    ],
+}
+
+
+
+export const Ddata = [
+  {
+    cover: "./images/discount/discount-1.png",
+    name: "BenuX 2022",
+    price: "$250",
+  },
+  {
+    cover: "./images/discount/discount-2.png",
+    name: "Sony TV 1080p",
+    price: "$450",
+  },
+  {
+    cover: "./images/discount/discount-3.png",
+    name: "Sony PS4",
+    price: "$50",
+  },
+  {
+    cover: "./images/discount/discount-4.png",
+    name: "Setgearr 2022",
+    price: "$100",
+  },
+  {
+    cover: "./images/discount/discount-5.png",
+    name: "Tony BGB",
+    price: "$20",
+  },
+  {
+    cover: "./images/discount/discount-6.png",
+    name: "RG products",
+    price: "$200",
+  },
+  {
+    cover: "./images/discount/discount-7.png",
+    name: "Ranasonic 2022",
+    price: "$300",
+  },
+  {
+    cover: "./images/discount/discount-8.png",
+    name: "Pune HD",
+    price: "$30",
+  },
+  {
+    cover: "./images/discount/discount-9.png",
+    name: "Sony CCTV",
+    price: "$80",
+  },
+]
+
+
+
+export const Sdata = [
+  {
+    id: 1,
+    title: "50% Off For Your First Shopping",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis lobortis consequat eu, quam etiam at quis ut convallis.",
+    cover: "./images/SlideCard/slide-1.png",
+  },
+  {
+    id: 2,
+    title: "50% Off For Your First Shopping",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis lobortis consequat eu, quam etiam at quis ut convallis.",
+    cover: "./images/SlideCard/slide-2.png",
+  },
+  {
+    id: 3,
+    title: "50% Off For Your First Shopping",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis lobortis consequat eu, quam etiam at quis ut convallis.",
+    cover: "./images/SlideCard/slide-3.png",
+  },
+  {
+    id: 4,
+    title: "50% Off For Your First Shopping",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis lobortis consequat eu, quam etiam at quis ut convallis.",
+    cover: "./images/SlideCard/slide-4.png",
+  },
+]
+
+
+
+export const Ndata = [
+  {
+    cover: "./images/arrivals/arrivals1.png",
+    name: "Sunglass",
+    price: "150",
+  },
+  {
+    cover: "./images/arrivals/arrivals2.png",
+    name: "Makeup",
+    price: "250",
+  },
+  {
+    cover: "./images/arrivals/arrivals3.png",
+    name: "Smart Watch",
+    price: "50",
+  },
+  {
+    cover: "./images/arrivals/arrivals4.png",
+    name: "Lipstick",
+    price: "15",
+  },
+  {
+    cover: "./images/arrivals/arrivals5.png",
+    name: "Green Plant",
+    price: "10",
+  },
+  {
+    cover: "./images/arrivals/arrivals6.png",
+    name: "Bonsai tree",
+    price: "400",
+  },
+]
+
+
+
+export const ShopData = {
+  shopItems: [
+    {
+      id: 7,
+      cover: "./images/shops/shops-1.png",
+      name: "Mapple Earphones",
+      price: "180",
+      discount: "25",
+    },
+    {
+      id: 8,
+      cover: "./images/shops/shops-2.png",
+      name: "Vivo android one",
+      price: "120",
+      discount: "10",
+    },
+    {
+      id: 9,
+      cover: "./images/shops/shops-3.png",
+      name: "Sony Light",
+      price: "20",
+      discount: "50 ",
+    },
+    {
+      id: 10,
+      cover: "./images/shops/shops-4.png",
+      name: "Iphone",
+      price: "999",
+      discount: "10 ",
+    },
+    // {
+    //   id: 11,
+    //   cover: "./images/shops/shops-5.png",
+    //   name: "Ceats wireless earphone",
+    //   price: "80",
+    //   discount: "20 ",
+    // },
+    // {
+    //   id: 12,
+    //   cover: "./images/shops/shops-7.png",
+    //   name: "Redimi Phone",
+    //   price: "400",
+    //   discount: "20 ",
+    // },
+    // {
+    //   id: 13,
+    //   cover: "./images/shops/shops-8.png",
+    //   name: "Xeats Bluetooth earphones",
+    //   price: "60",
+    //   discount: "5 ",
+    // },
+    // {
+    //   id: 14,
+    //   cover: "./images/shops/shops-9.png",
+    //   name: "Airpod",
+    //   price: "120",
+    //   discount: "10",
+    // },
+    // {
+    //   id: 15,
+    //   cover: "./images/shops/shops-10.png",
+    //   name: "Silver Cap",
+    //   price: "5",
+    //   discount: "2",
+    // },
+  ],
+}
+
+
+
+
+export const Tdata = [
+  {
+    cover: "./images/top/category-1.png",
+    para: "headphone",
+    desc: "3k orders this week",
+  },
+  {
+    cover: "./images/top/category-2.png",
+    para: "watch",
+    desc: "4k orders this week",
+  },
+  {
+    cover: "./images/top/category-3.png",
+    para: "sunglass",
+    desc: "6k orders this week",
+  },
+  {
+    cover: "./images/top/category-2.png",
+    para: "watch",
+    desc: "4k orders this week",
+  },
+  {
+    cover: "./images/top/category-3.png",
+    para: "sunglass",
+    desc: "6k orders this week",
+  },
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
